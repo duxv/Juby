6666135232a05f3e3a3f7b484adbebf286816d69
David <mrcakee17@gmail.com>
2021-06-21 21:47:38 +0300

    :^)

--- read/read.go
+++ read/read.go
@@ -4,12 +4,54 @@ import (
 	"encoding/json"
 	"errors"
 	"io/ioutil"
+	"strings"
 )
+
+type Language struct {
+	Name     string
+	Score    int
+	Keywords []string
+}
 
 type Extension struct {
 	Name       string
 	Type       string
 	Extensions []string
+}
+
+func Contains(key string, substrings []string) bool {
+	for _, r := range substrings {
+		if strings.Contains(key, r) {
+			return true
+		}
+	}
+	return false
+}
+
+func FindLanguage(content string) error {
+	var langs []Language
+	var inString string
+	var quotes = []string{"'", "\""}
+	keywords, err := Readfile("./assets/keywords.json")
+	if err != nil {
+		return err
+	}
+	json.Unmarshal([]byte(keywords), &langs)
+	for _, key := range strings.Split(content, " ") {
+		if inString == "" && Contains(key, quotes) {
+			inString = string(key[0])
+			continue
+		}
+		if inString != "" && strings.Contains(key, inString) {
+			inString = ""
+			continue
+		}
+		if inString != "" {
+			continue
+		}
+		// TODO: check all programming languages and do an algorithm to decide which programming language is
+	}
+	return nil
 }
 
 func Readfile(filepath string) (res string, er error) {

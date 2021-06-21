ed05251de821eb5b3b55b950f346d2d7e972593a
David <mrcakee17@gmail.com>
2021-06-21 21:18:35 +0300

    added keywords

--- replace.py
+++
@@ -1,7 +0,0 @@
-y = ["Begin", "Break", "Catch", "Class", "Continue", "Data", "Define", "Do", "DynamicParam", "Else ", " Elseif ", " End ", " Enum ", " Exit ", " Filter ", " Finally ", " For ", " ForEach ", " From ", " Function ", " Hidden ", " If ", " In ", " InlineScript ", " Param ", " Process ", " Return ", " Static ", " Switch ", " Throw ", " Trap ", " Try ", " Until ", " Using ", " Var ", "While"]
-
-z = []
-for i in y:
-    z.append(i.replace(" ", ""))
-
-print(z)
